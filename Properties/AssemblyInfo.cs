fc53f6e69b827caee87332fa3dabd7036651da3d
edyrol <43393859+edyrol@users.noreply.github.com>
2019-05-20 12:46:22 -0500

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("My-Heartrhstone-Collection")]
+[assembly: AssemblyTitle("My-Hearthstone-Collection")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("My-Heartrhstone-Collection")]
+[assembly: AssemblyProduct("My-Hearthstone-Collection")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
